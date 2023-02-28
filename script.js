10e2b5d05fb0357ef41bca4cb245bba41e2f830d
Gerald Esteban <wakin.esteban@gmail.com>
2023-02-28 14:25:35 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// 'user strict';
+
+// const name = prompt('Enter your name');
+
+// const math = +prompt('Math: ');
+// const science = +prompt('Science: ');
+// const english = +prompt('English: ');
+// const pe = +prompt('PE: ');
+// const filipino = +prompt('Filipino: ');
+
+// const average = (math + science + english + pe + filipino) / 5;
+
+// const failedSubject = [];
+// math <= 75 ? failedSubject.push('Math') : '';
+// science <= 75 ? failedSubject.push('Science') : '';
+// english <= 75 ? failedSubject.push('English') : '';
+// english <= 75 ? failedSubject.push('PE') : '';
+// filipino <= 75 ? failedSubject.push('Filipino') : '';
+
+// console.log(failedSubject);
+
+// console.log(`Average: ${average}`);
+
+// if (
+//   average >= 80 &&
+//   math <= 75 &&
+//   science <= 75 &&
+//   english <= 75 &&
+//   english <= 75 &&
+//   filipino <= 75
+// ) {
+//   console.log(
+//     `Sorry you failed ${failedSubject.join(', ')} but you passed the course.`
+//   );
+// } else if (average >= 80) {
+//   console.log(`You passed the course.`);
+// } else {
+//   console.log(
+//     `Sorry you failed ${failedSubject.join(', ')} and you failed the course.`
+//   );
+// }
